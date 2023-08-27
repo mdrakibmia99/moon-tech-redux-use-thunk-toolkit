07c321dac6d56a7dedf5044b19cebabc1d195833
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-08-27 20:28:42 +0600

    connect redux successfully

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import ProductCard from "../components/ProductCard";
+import { useSelector } from "react-redux";
 
 const Home = () => {
   const [products,setProducts]=useState([])
@@ -8,7 +9,9 @@ const Home = () => {
     .then(res=>res.json())
     .then(data=>setProducts(data))
   },[])
-  console.table(products);
+  console.log(products);
+  const state=useSelector((state)=>state)
+  console.log(state)
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
       {
--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,6 +1,8 @@
-const initialState={};
+const initialState={
+    test:"test store"
+};
 const productReducer = (state=initialState,action) => {
-    return ;
+    return state;
 };
 
 export default productReducer;
