b2a7f7731775d8fec50f73fe15356b248d93e2ee
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2023-08-27 21:05:38 +0600

    add composeWithDevTools() use for stataus check

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BiListPlus } from "react-icons/bi";
 import { useDispatch } from "react-redux";
 import { ADD_TO_CART } from "../redux/actionTypes/actionTypes";
+import { addToCart } from "../redux/actionCreators/productAction";
 
 const ProductCard = ({ product }) => {
   // initilize dispatch funciton
@@ -25,7 +26,7 @@ const ProductCard = ({ product }) => {
       </div>
       <div className='flex gap-2 mt-5'>
         <button
-         onClick={()=>dispatch({type:ADD_TO_CART,payload:product})}
+         onClick={()=>dispatch(addToCart(product))}
         className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'>
           Add to cart
         </button>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import {createStore} from "redux";
 import productReducer from "./reducers/productReducer";
+import { composeWithDevTools } from "@redux-devtools/extension";
 // create  a store 
-const store=createStore(productReducer);
+const store=createStore(productReducer,composeWithDevTools());
 export default store;
